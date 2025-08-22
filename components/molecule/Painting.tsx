ef472b0ce0c9ece33f8b2970208e39ec0ee45ca7
Vitaliy Dyachuk <shadespiritenator@gmail.com>
2025-08-22 12:33:20 +0300

    Update Painting.tsx

--- components/molecule/Painting.tsx
+++ components/molecule/Painting.tsx
@@ -12,7 +12,7 @@ export default function Painting({ data }: PaintingProps) {
     return (
         <div className="flex justify-center items-center mb-4">
             <div className="relative w-full">
-                <div className={`flex justify-center items-center bg-gray-200 p-2 rounded-lg shadow-lg ${styles.container}`}>
+                <div className={`flex justify-center items-center  p-2 rounded-lg shadow-lg ${styles.container}`}>
                     <Image
                         className={`bg-gray-200 rounded-lg shadow-lg ${styles.img}`}
                         src={data.img}
